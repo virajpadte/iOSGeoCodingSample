c42dee9c0bab138c896947d0ea0816ffb5d775f7
Viraj Padte <virajpadte@gmail.com>
2017-01-27 14:54:39 -0700

    Centered current location map on add request segue to maps view controller

--- iOSGeoCodingSample/iOSGeoCodingSample/ViewController.swift
+++ iOSGeoCodingSample/iOSGeoCodingSample/ViewController.swift
@@ -7,17 +7,49 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,CLLocationManagerDelegate,MKMapViewDelegate{
 
+    @IBOutlet weak var map: MKMapView!
+    
+    let locMan = CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        locMan.delegate = self
+        locMan.desiredAccuracy =  kCLLocationAccuracyBest
+        locMan.requestWhenInUseAuthorization()
+        locMan.startUpdatingLocation()
+        
+        let longPress = UILongPressGestureRecognizer(target: self, action: #selector(ViewController.longPressed(getGestureInformation:)))
+        longPress.minimumPressDuration = 2
+        map.addGestureRecognizer(longPress)
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let currentLocation = locations[0].coordinate
+        print(currentLocation)
+        let region = MKCoordinateRegion(center: currentLocation, span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
+        map.setRegion(region, animated: true)
+        
+        //add annotation just to check if this works
+        //let annotation = MKPointAnnotation()
+        //annotation.coordinate = currentLocation
+        //map.addAnnotation(annotation)
+    }
+    
+    func longPressed(getGestureInformation: UIGestureRecognizer){
+        if getGestureInformation.state == UIGestureRecognizerState.began{
+            print("detected")
+        }
+        
     }
 
 
